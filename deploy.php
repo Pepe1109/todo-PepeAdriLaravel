e11274302ad4cbed99528091ceb115edcc7dfb5c
Pepe1109 <palemanyfct@gmail.com>
2025-12-04 19:50:13 +0100

    [NEW] Included deployer.php

--- deploy.php
+++ deploy.php
@@ -5,7 +5,7 @@ require 'recipe/laravel.php';
 
 // Config
 
-set('repository', 'https://github.com/acoloma-edu/todo.git');
+set('repository', 'yes');
 
 add('shared_files', []);
 add('shared_dirs', []);
@@ -13,19 +13,7 @@ add('writable_dirs', []);
 
 // Hosts
 
-host('192.168.1.137')
-    ->set('remote_user', 'ddaw-ud4-deployer')
-    ->set('deploy_path', '/var/www/ddaw-ud4-a4/html');
 
 // Hooks
+
 after('deploy:failed', 'deploy:unlock');
-
-task('build', function () {
-    run('cd {{release_path}} && build');
-});
-
-task('reiniciar-fpm', function () {
-    run('sudo /etc/init.d/php8.1-fpm restart');
-});
-
-after('deploy', 'reiniciar-fpm');
