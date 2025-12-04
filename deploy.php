e2fafa1cb5afcdaa77c1c8eb9d086bd83bb3f388
Pepe1109 <palemanyfct@gmail.com>
2025-12-04 20:03:42 +0100

    [MOD] deploy.php new config

--- deploy.php
+++ deploy.php
@@ -5,7 +5,7 @@ require 'recipe/laravel.php';
 
 // Config
 
-set('repository', 'yes');
+set('repository', 'https://github.com/Pepe1109/todo-PepeAdriLaravel.git');
 
 add('shared_files', []);
 add('shared_dirs', []);
@@ -13,6 +13,9 @@ add('writable_dirs', []);
 
 // Hosts
 
+host('3.238.98.12')
+    ->set('remote_user', 'ddaw-ud4-deployer')
+    ->set('deploy_path', '/var/www/ddaw-ud4-a4/html');
 
 // Hooks
 
