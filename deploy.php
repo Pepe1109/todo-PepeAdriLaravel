0f4848e45f0a1b54a6800bc53335c60dbc89e145
Pepe1109 <palemanyfct@gmail.com>
2025-12-12 17:27:07 +0100

    Corregir deploy.php para AWS

--- deploy.php
+++ deploy.php
@@ -5,27 +5,31 @@ require 'recipe/laravel.php';
 
 // Config
 set('application', 'todo-PepeAdriLaravel');
-
 set('repository', 'https://github.com/Pepe1109/todo-PepeAdriLaravel.git');
-
-add('shared_files', ['']);
-add('shared_dirs', ['']);
-add('writable_dirs', []);
-
 set('git_tty', true);
 
-// Hosts
+add('shared_files', []); 
+add('shared_dirs', []);
+add('writable_dirs', []);
 
-host('3.238.98.126')
+// Hosts
+host('3.238.119.243')
     ->set('remote_user', 'ddaw-ud4-deployer')
-    ->set('deploy_path', '/var/www/ddaw-ud4-a4/html');
+    ->set('deploy_path', '/var/www/ddaw-ud4-a4/html')
+    ->set('ssh_multiplexing', false);
 
 // Hooks
 
 task('build', function () {
-run('cd {{release_path}} && build');
+    run('cd {{release_path}} && build');
+});
+
+task('reload:php-fpm', function () {
+    run('sudo /etc/init.d/php8.3-fpm restart');
 });
 
 after('deploy:failed', 'deploy:unlock');
 
 before('deploy:symlink', 'artisan:migrate');
+
+after('deploy', 'reload:php-fpm');
